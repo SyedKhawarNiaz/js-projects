b38f2f11c498c2f9a11c51f554af6a715ef6b264
Syed Khawar <93276430+SyedKhawarNiaz@users.noreply.github.com>
2024-05-16 17:50:33 +0500

    finnal

---
+++ Password Checker/script.js
@@ -0,0 +1,23 @@
+let password = document.querySelector("#pass");
+let show = document.querySelector(".hightlight");
+
+password.addEventListener("input", () => {
+  let passvalue = password.value;
+  if (passvalue.length < 8) {
+    show.style.color = "red";
+    show.textContent = "Length is smaller than 8 characters";
+  } else if (
+    passvalue.search(/[a-z]/) === -1 ||
+    passvalue.search(/[A-Z]/) === -1
+  ) {
+    show.style.color = "red";
+    show.textContent =
+      "Password must contain both lowercase and uppercase letters";
+  } else if (passvalue.search(/[0-9]/) == -1) {
+    show.style.color = "red";
+    show.textContent = "Password must contain Numeric characters";
+  } else {
+    show.style.color = "black";
+    show.textContent = "Strong Password*"
+  }
+});
